783aaefb305f5f0b4a3075c5b87e94a60894bd61
Agnes Kavata Musyoka <kavatamusyoka24@gmail.com>
2019-05-31 13:40:07 +0300

    children widget as to pass data in json file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart'as http;
+import 'dart:convert';
 void main()=> runApp(new MaterialApp(
   theme: new ThemeData(
     primarySwatch: Colors.teal,
@@ -10,6 +12,7 @@ void main()=> runApp(new MaterialApp(
 class HomePage extends StatefulWidget{
   @override
   HomePageState createState()=> new HomePageState();
+  List data;
 
 }
 class HomePageState extends State<HomePage>{
@@ -23,7 +26,34 @@ class HomePageState extends State<HomePage>{
       body: new Container(
         child: new Center(
           child: new FutureBuilder(
-            future: DefaultAssetBundle.of(context),
+            future: DefaultAssetBundle
+          .of(context)
+          .loadString('load_json/person.json'),
+            builder: (context, snapshort){
+              //Decode Json
+              var mydata = json.decode(snapshort.data.toString());
+
+              return new ListView.builder(
+                itemBuilder: (BuildContext context, int index){
+                  return new Card(
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        new Text("Name:" + mydata [index]['name']),
+                        new Text("Age:" + mydata [index]['age']),
+                        new Text("Height:" + mydata [index]['height']),
+                        new Text("Weight:" + mydata [index]['weight']),
+                        new Text("Gender:" + mydata [index]['gender']),
+                        new Text("HairColour:" + mydata [index]['hair_colour']),
+
+                      ],
+                    ),
+                  );
+                  },
+                itemCount: mydata == null ? 0 :mydata.length,
+
+              );
+            },
           ),
         ),
       ),
